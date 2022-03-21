0dcafe54ee9b8425c51356e750ca31db0fda2c68
Unevilicorn <unevilicorn@gmail.com>
2022-03-21 01:24:31 +0000

    fix free shop item and update using new callback

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import {
   getPlayers,
   getRandom,
   getRandomArrayElement,
-  isCollectiblePreviouslySeen,
   isQuestCollectible,
+  ModCallbacksCustom,
   upgradeMod,
 } from "isaacscript-common";
 
@@ -13,7 +13,7 @@ const MOD_NAME = "Amnesia";
 const AMNESIA_COLLECTIBLE_TYPE = Isaac.GetItemIdByName("Amnesia");
 
 const AMNESIA_CONFIG = {
-  BASE_CHANCE: 0.25,
+  BASE_CHANCE: 1,
   STACKABLE: false,
 };
 
@@ -25,15 +25,16 @@ export function main(): void {
   const modVanilla = RegisterMod(MOD_NAME, 1);
   const mod = upgradeMod(modVanilla);
 
-  mod.AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, postPickupInit);
+  mod.AddCallbackCustom(ModCallbacksCustom.MC_POST_COLLECTIBLE_INIT_FIRST, postPickupInitCollectible);
 }
 
-function postPickupInit(entityPickup: EntityPickup) {
+function postPickupInitCollectible(entityPickup: EntityPickup) {
+  // printConsole(entityPickup.SubType.toString());
   for (const player of getPlayers()) {
     if (checkCanMorph(player, entityPickup)) {
       modState.isMorphing = true;
       const newMorphItemId = pickItem(player, entityPickup.SubType);
-      entityPickup.Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, newMorphItemId);
+      entityPickup.Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, newMorphItemId, true);
       modState.isMorphing = false;
     }
   }
@@ -41,11 +42,7 @@ function postPickupInit(entityPickup: EntityPickup) {
 
 function checkCanMorph(player: EntityPlayer, entityPickup: EntityPickup): Boolean {
   return (
-    !modState.isMorphing &&
-    player.HasCollectible(AMNESIA_COLLECTIBLE_TYPE) &&
-    entityPickup.Variant == PickupVariant.PICKUP_COLLECTIBLE &&
-    !isQuestCollectible(entityPickup.SubType) &&
-    !isCollectiblePreviouslySeen(entityPickup)
+    !modState.isMorphing && player.HasCollectible(AMNESIA_COLLECTIBLE_TYPE) && !isQuestCollectible(entityPickup.SubType)
   );
 }
 
